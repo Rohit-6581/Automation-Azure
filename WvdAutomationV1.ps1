302245ddaf861b7793d90feb1f857496338c5a7a
Rohit Raj <45617500+Rohit-6581@users.noreply.github.com>
2022-01-02 20:31:26 +0530

    Add files via upload
    Change VM time as per working hours defined in tags associated with the WVD host pool.

---
+++ WvdAutomationV1.ps1
@@ -0,0 +1,55 @@
+﻿#Main Script
+
+$hostpoolsInSub = Get-AzWvdHostPool
+$x = Get-AzTag  -ResourceId $hostpoolsInSub[1].Id
+$tagloop = @()
+$startTime = ""
+$endTime = ""
+$intendTime = 0
+$intstartTime = 0
+$tagloop= $x.Properties.TagsProperty
+$startTime = $tagloop.Get_Item("StartTime")
+$endTime = $tagloop.Get_Item("EndTime")
+
+$startTime = $startTime.Replace(" ","")
+$endTime = $endTime.Replace(" ","")
+
+if($startTime.Contains("P")){
+         $startTime = $startTime.Replace("PM","")
+         $intstartTime = [int]$startTime
+         $intstartTime = $intstartTime + 12
+ }else{
+    $startTime = $startTime.Replace("AM","")
+    $intstartTime = [int]$startTime    
+    }
+
+if($endTime.Contains("P")){
+        $endTime = $endTime.Replace("PM","")
+        $intendTime = [int]$endTime
+        $intendTime = $intendTime + 12
+
+}else{
+    $endTime = $endTime.Replace("AM","")
+    $intendTime = [int]$endTime    
+    }
+    
+
+
+
+
+$tDate =(Get-Date).ToUniversalTime()
+$tz = [System.TimeZoneInfo]::FindSystemTimeZoneById("Eastern Standard Time")
+$tCurrentTime = [System.TimeZoneInfo]::ConvertTimeFromUtc($tDate, $tz)
+
+$IntcurrTime = [int]$tCurrentTime.Hour
+
+
+#Stuff where my math comes in
+$intstartTime
+$intendTime
+
+
+if($IntcurrTime -ge $intstartTime -and $IntcurrTime -le $intendTime){
+    Write-Output("Change the VM state")
+}
+
