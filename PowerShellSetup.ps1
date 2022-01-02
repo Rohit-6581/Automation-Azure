8a3e0f4f80effd5d5827e4cddc8f33b98a652886
Rohit Raj <45617500+Rohit-6581@users.noreply.github.com>
2022-01-02 19:46:56 +0530

    Add files via upload
    Powershell setup and necessary commands

---
+++ PowerShellSetup.ps1
@@ -0,0 +1,15 @@
+﻿$PSVersionTable.PSVersion
+Get-ExecutionPolicy
+Set-ExecutionPolicy Unrestricted
+#Install-Module -Name Az -Force -AllowClobber
+Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+Import-Module Az
+(Get-Module).Name
+Connect-AzAccount
+(Get-AzContext).Name
+
+Connect-AzAccount -Tenant 'XXX' -SubscriptionId 'YYYY'
+
+
+
+Disconnect-AzAccount
